828a659634a95545b39a9a230f0ef36bb04aac9e
Alexander Vourtsis <alexander@vourtsis.com>
2018-10-22 20:57:21 +0300

    Updated CS files
    Camera.cs added the ways on how to setup, get basic info, zoom, fill a sprite to camera view.
    Model.cs changed flip normals as a script & also added one to expose sorting layers for MeshRenderer

--- Camera.cs
+++ Camera.cs
@@ -9,9 +9,70 @@ using UnityEngine;
 
 
 /* -----------------------------------------
+   Setup Camera
+----------------------------------------- */
+
+/* 
+
+    1. Select "Game" tab, add a couple more Resolutions in the dropdown at the top left
+       Add 16:9 Landscape & 22:10 Landscape
+    
+    2. Click on your Camera (usually named "Main Camera")
+       Change Orthographic Size, 5.4 is proper for full view in the above aspect ratios
+	
+ */
+
+
+/* -----------------------------------------
    Find Main Camera
 ----------------------------------------- */
 Camera mainCam = GameObject.Find("Main Camera").GetComponent<Camera>();
+
+
+/* -----------------------------------------
+   Get Camera Info
+----------------------------------------- */
+/// Class Body:
+Camera mainCam;
+
+/// Start(), Awake():
+mainCam = GameObject.Find("Main Camera").GetComponent<Camera>();
+
+/// Start(), Awake(), Update():
+
+// get size in pixels
+int pixelWidth  = mainCam.pixelWidth;
+int pixelHeight = mainCam.pixelHeight;
+
+// get size in pixels (account for scaling)
+int pixelWidth  = mainCam.scaledPixelWidth;
+int pixelHeight = mainCam.scaledPixelHeight;
+
+// get size in units
+float unitHeight = mainCam.orthographicSize;
+float unitWidth  = unitHeight * mainCam.aspect;
+
+// get aspect ratio (ratio = (width / height))
+float aspectRatio = mainCam.aspect;
+
+
+/* -----------------------------------------
+   Zoom Camera
+----------------------------------------- */
+/// Class Body:
+Camera mainCam;
+float baseZoom;
+float zoom;
+
+/// Start(), Awake():
+mainCam  = GameObject.Find("Main Camera").GetComponent<Camera>();
+baseZoom = mainCam.orthographicSize; // base zoom, usually 5.4
+
+/// Start(), Awake(), Update():
+zoom = 1.0f; // change this to zoom
+
+// setting orthographicSize makes it an easy way to zoom in and out
+mainCam.orthographicSize = baseZoom * zoom;
 
 
 /* -----------------------------------------
@@ -37,4 +98,34 @@ gameObject.transform.RotateAround(point, new Vector3(0.0f, 1.0f, 0.0f), 20 * Tim
 // Update():
 // make GameObject's sprite always face the camera
 gameObject.transform.LookAt(GameObject.Find("Main Camera").transform.position, Vector3.up);
+
+
+/* -----------------------------------------
+   Sprite Fill Camera View
+----------------------------------------- */
+/// Class Body:
+Camera mainCam;
+SpriteRenderer spriteRenderer;
+
+/// Start(), Awake():
+mainCam = GameObject.Find("Main Camera").GetComponent<Camera>();
+spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+
+/// Start(), Update():
+float cameraHeight = mainCam.orthographicSize * 2;
+Vector2 cameraSize = new Vector2(mainCam.aspect * cameraHeight, cameraHeight);
+Vector2 spriteSize = spriteRenderer.sprite.bounds.size;
+
+Vector2 scale = transform.localScale;
+
+if (cameraSize.x >= cameraSize.y) {
+    // landscape
+    scale *= cameraSize.x / spriteSize.x;
+} else {
+    // portrait
+    scale *= cameraSize.y / spriteSize.y;
+}
+
+gameObject.transform.position   = Vector2.zero; // move to world 0,0 (optional)
+gameObject.transform.localScale = scale; // apply scale
 
--- Model.cs
+++ Model.cs
@@ -17,7 +17,7 @@ gameObject.GetComponent<MeshRenderer>().enabled = false;
 
 
 /* -----------------------------------------
-   Flip Normals
+   Flip Mesh Normals
 ----------------------------------------- */
 
 /*
@@ -25,36 +25,146 @@ gameObject.GetComponent<MeshRenderer>().enabled = false;
     note: This is required in order to display a texture on a model on the inside and not outside
           e.g. a skybox, sphere or cylinder
 
- */
+*/
 
-// requires a MeshFilter component [RequireComponent(typeof(MeshFilter))]
-// Start():
-MeshFilter filter = GetComponent(typeof (MeshFilter)) as MeshFilter;
+// -------------( MeshFlipNormals.cs )--------------
+ 
+using UnityEngine;
+using System.Collections;
 
-if (filter != null) {
+[RequireComponent(typeof(MeshFilter))]
+public class MeshFlipNormals : MonoBehaviour {
+ 
+	void Awake() {
+		
+		MeshFilter filter = GetComponent<MeshFilter>();
+		
+		if (filter != null) {
+			
+			Mesh mesh = filter.mesh;
+ 
+			Vector3[] normals = mesh.normals;
+			
+			for (int i = 0; i < normals.Length; i++) {
+				normals[i] = -normals[i];
+			}
+			
+			mesh.normals = normals;
+ 
+			for (int m = 0; m < mesh.subMeshCount; m++) {
+				
+				int[] triangles = mesh.GetTriangles(m);
+				
+				for (int i = 0; i < triangles.Length; i+=3) {
+					
+					int temp = triangles[i + 0];
+					triangles[i + 0] = triangles[i + 1];
+					triangles[i + 1] = temp;
+					
+				}
+				
+				mesh.SetTriangles(triangles, m);
+			}
+			
+		}		
+	}
+}
 
-    Mesh mesh = filter.mesh;
+// -------------( MeshFlipNormals.cs )--------------
 
-    Vector3[] normals = mesh.normals;
 
-    for (int i = 0; i < normals.Length; i++) {
-        normals[i] = -normals[i];
-    }
+/* -----------------------------------------
+   Change Mesh Sorting Layer
+----------------------------------------- */
+/*
 
-    mesh.normals = normals;
+	note: Unity does not have Sorting Layer & Order exposed in the editor for MeshRenderer, 
+          as a result this makes harder to mix 2D and 3D in some cases (e.g. TextMeshPro text) from within the Editor
+          Save the below script inside an "Editor" folder and it will add fields for 2D Sort layer & order
 
-    for (int m = 0; m < mesh.subMeshCount; m++) {
+	
+*/
 
-        int[] triangles = mesh.GetTriangles(m);
+// -------------( MeshSortingLayer.cs )--------------  
 
-        for (int i=0; i < triangles.Length; i += 3) {
-            int temp = triangles[i + 0];
-            triangles[i + 0] = triangles[i + 1];
-            triangles[i + 1] = temp;
+using System;
+using System.Reflection;
+using UnityEngine;
+using UnityEditor;
+using UnityEditorInternal;
+
+[CustomEditor(typeof(MeshRenderer))]
+public class MeshSortingLayer : Editor
+{
+
+    public override void OnInspectorGUI()
+    {
+
+        serializedObject.Update();
+
+        DrawDefaultInspector();
+
+        EditorGUILayout.Space();
+
+        SerializedProperty sortingLayerID = serializedObject.FindProperty("m_SortingLayerID");
+        SerializedProperty sortingOrder   = serializedObject.FindProperty("m_SortingOrder");
+
+        EditorGUILayout.BeginVertical();
+
+        string[] layerNames = GetSortingLayerNames();
+        int[]    layerID    = GetSortingLayerUniqueIDs();
+
+        int selected = -1;
+        int sID = sortingLayerID.intValue;
+
+        for (int i = 0; i < layerID.Length; i++) {
+
+            if (sID == layerID[i]) {
+                selected = i;
+            }
+
         }
 
-        mesh.SetTriangles(triangles, m);
+        if (selected == -1) {
+
+            for (int i = 0; i < layerID.Length; i++) {
+
+                if (layerID[i] == 0) {
+                    selected = i;
+                }
+
+            }
+
+        }
+
+        selected                = EditorGUILayout.Popup("Sorting Layer", selected, layerNames);
+        sortingLayerID.intValue = layerID[selected];
+		
+        EditorGUILayout.PropertyField(sortingOrder, new GUIContent("Order in Layer"));
+
+        EditorGUILayout.EndVertical();
+
+        serializedObject.ApplyModifiedProperties();
+
+    }
+
+    public string[] GetSortingLayerNames() {
+		
+        Type internalEditorUtilityType     = typeof(InternalEditorUtility);
+        PropertyInfo sortingLayersProperty = internalEditorUtilityType.GetProperty("sortingLayerNames", BindingFlags.Static | BindingFlags.NonPublic);
+        return (string[])sortingLayersProperty.GetValue(null, new object[0]);
+		
+    }
+
+    public int[] GetSortingLayerUniqueIDs() {
+		
+        Type internalEditorUtilityType             = typeof(InternalEditorUtility);
+        PropertyInfo sortingLayerUniqueIDsProperty = internalEditorUtilityType.GetProperty("sortingLayerUniqueIDs", BindingFlags.Static | BindingFlags.NonPublic);
+        return (int[])sortingLayerUniqueIDsProperty.GetValue(null, new object[0]);
+		
     }
 
 }
+
+// -------------( MeshSortingLayer.cs )--------------  
 
