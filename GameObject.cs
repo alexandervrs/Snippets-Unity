8c4550601eb6f58f27067cd3c0cb42d40aa18f1f
Alexander Vourtsis <alexander@vourtsis.com>
2018-11-13 02:02:03 +0200

    Updated CS files
    Updated GameObject.cs to include info on how to get all gameobjects in runtime and Editor, also info about getting/setting useful gameobject properties

--- GameObject.cs
+++ GameObject.cs
@@ -6,6 +6,7 @@
 
 /* using */
 using UnityEngine;
+using UnityEngine.SceneManagement; // only for gameObject.scene
 
 
 /* -----------------------------------------
@@ -39,38 +40,100 @@ newObj.transform.SetParent(gameObject.transform, true);
 /* -----------------------------------------
    Find GameObject
 ----------------------------------------- */
-// get handle of current GameObject
+/* get handle of current GameObject that is executing the script */
 GameObject obj = gameObject;
 
-// find first GameObject by name
+/* find first GameObject by name */
 GameObject obj = GameObject.Find("Test");
 
-// find all GameObjects by name (slow)
+/* find all GameObjects by name (slow) */
 GameObject[] objectsOfType = GameObject.FindObjectsOfType<GameObject>();
 foreach (GameObject thisGameObject in objectsOfType) {
             
-	if (thisGameObject.name == "Test") {
+	if (thisGameObject.scene.isLoaded && thisGameObject.activeInHierarchy && thisGameObject.name == "Test") {
 
+		Debug.Log(thisGameObject.name);
 		// do stuff with thisGameObject ...
 
 	}
 }
 
-// find all GameObjects by Type
+/* find all GameObjects by Type */
 dynamic[] objectList = FindObjectsOfType<Light>();
 // check objectList.Length for number of Light objects found
- 
 
-/* Manage Tags: Edit > Project Settings > Tags and Layers */
-
-// find any GameObject with tag
+/* find any GameObject with Tag */
+// note: Manage Tags: Edit > Project Settings > Tags and Layers
 GameObject obj = GameObject.FindWithTag("Enemies");
 
-// find all GameObjects with tag
+/* find all GameObjects with Tag */
 GameObject[] taggedObj = GameObject.FindGameObjectsWithTag("Enemies");
 foreach (GameObject obj in taggedObj) {
 	GameObject.Instantiate(prefab, obj.transform.position, obj.transform.rotation);
 }
+
+/* find all GameObjects in Scene */
+// define a method in Class Body
+private List<GameObject> GetAllActiveObjectsInScene() {
+
+	List<GameObject> objectsInScene = new List<GameObject>();
+	GameObject[] allObjects = UnityEngine.Object.FindObjectsOfType<GameObject>() ;
+
+	foreach (GameObject thisGameObject in allObjects) {
+
+		if (thisGameObject.scene.isLoaded && thisGameObject.activeInHierarchy) {
+
+			Debug.Log(thisGameObject.name);
+			objectsInScene.Add(thisGameObject);
+
+		}
+
+	}
+
+	return objectsInScene;
+
+}
+
+// retrieve all GameObjects in a list by calling the above method
+List<GameObject> gameobjectList = GetAllActiveObjectsInScene();
+
+foreach (GameObject thisGameObject in gameobjectList) {
+	Debug.Log(thisGameObject.name);
+}
+
+
+/* find all GameObjects in Scene (even Inactive ones) (Editor Only) */
+#ifdef UNITY_EDITOR
+
+// define a method in Class Body
+private List<GameObject> GetAllObjectsInScene()
+{
+	List<GameObject> objectsInScene = new List<GameObject>();
+
+	foreach (GameObject thisGameObject in Resources.FindObjectsOfTypeAll(typeof(GameObject)) as GameObject[]) {
+		
+		if (thisGameObject.hideFlags == HideFlags.NotEditable || thisGameObject.hideFlags == HideFlags.HideAndDontSave) {
+			continue;
+		}
+		
+		if (!UnityEditor.EditorUtility.IsPersistent(thisGameObject.transform.root.gameObject)) {
+			continue;
+		}
+
+		objectsInScene.Add(thisGameObject);
+	}
+
+	return objectsInScene;
+}
+
+// retrieve all GameObjects in a list by calling the above method
+List<GameObject> gameobjectList = GetAllObjectsInScene();
+
+foreach (GameObject thisGameObject in gameobjectList) {
+	Debug.Log(thisGameObject.name);
+}
+
+#endif
 
 
 /* -----------------------------------------
@@ -97,14 +160,49 @@ GameObject.DontDestroyOnLoad(gameObject);
 
 
 /* -----------------------------------------
+   Handle GameObject Properties
+----------------------------------------- */
+// get the GameObject's name
+string objName = gameObject.name;
+
+// get the GameObject's tag
+string objTag = gameObject.tag;
+
+// get the GameObject's layer
+int objLayer = gameObject.layer;
+Debug.Log( "GameObject Layer is: "+LayerMask.LayerToName(objLayer) );
+
+// set the GameObject's layer by name
+gameObject.layer = LayerMask.NameToLayer("Water");
+
+// return the Scene the GameObject belongs to
+Scene objScene = gameObject.scene;
+
+// set GameObject to be active or inactive in scene, execute events, be accessible from other objects etc.
+gameObject.SetActive(true);
+
+// get if GameObject is active in a loaded scene
+bool isActive = (gameObject.scene.isLoaded && gameObject.activeInHierarchy);
+
+// change the parent object transform and make gameObject into a child in the Hierarchy under "someOtherGameObject"
+gameObject.transform.SetParent(someOtherGameObject.transform, true); // "true" is WorldPositionStays, keeps world position, rotation, scale instead
+
+
+/* -----------------------------------------
    Handle Components
 ----------------------------------------- */
 // add a component
-SphereCollider sc = gameObject.AddComponent<SphereCollider>() as SphereCollider;
+SphereCollider component = gameObject.AddComponent<SphereCollider>() as SphereCollider;
 
 // get an existing component handle
-SphereCollider sc = gameObject.GetComponent<SphereCollider>();
+SphereCollider component = gameObject.GetComponent<SphereCollider>();
 
 // remove a component
 GameObject.Destroy(gameObject.GetComponent<SphereCollider>());
+
+// check if a component exists
+SphereCollider component = gameObject.AddComponent<SphereCollider>() as SphereCollider;
+if (component == null) {
+	// Component doesn't exist on this GameObject ...
+}
 
