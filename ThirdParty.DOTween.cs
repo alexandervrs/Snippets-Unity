425dfc81aabf1b4abb9771746ec25290170a797b
alexandervrs <alexander@vourtsis.com>
2019-09-02 23:13:09 +0300

    Update ThirdParty.DOTween.cs

--- ThirdParty.DOTween.cs
+++ ThirdParty.DOTween.cs
@@ -70,7 +70,7 @@ gameObject.transform.DOScale(new Vector3(
 ), 1.5f).SetEase(Ease.OutSine);
 
 // animate alpha
-// use either <Renderer>, <SpriteRenderer> or <MeshRenderer> based on what you want to fade
+// use either <Renderer>, <SpriteRenderer>, <MeshRenderer>, <Image> based on what you want to fade
 gameObject.transform.GetComponent<Renderer>().material.DOFade(0.5f, 1.2f).SetEase(Ease.OutSine).OnComplete(() => { // DOFade(endValue, duration)
 	// fade complete ...
 });
@@ -176,6 +176,16 @@ sequence.InsertCallback(2.3f, () => {
 	// Sequence Callback at 2.3s ..." 
 });
 
+// execute a callback on complete of one loop cycle
+sequence.OnStepComplete(() => { 
+	// Sequence completed one loop cycle ...
+});
+
+// execute a callback on complete of the entire sequence
+sequence.OnComplete(() => { 
+	// Sequence on completed (includes loops) ...
+});
+
 sequence.SetLoops(-1); // loop endlessly
 sequence.Play(); // play the entire sequence
 
@@ -278,6 +288,9 @@ tween.Rewind();
 
 // rewinds the tween/sequence, pauses it and includes any delay with SetDelay()
 tween.Rewind(true);
+
+// plays/resumes the tween/sequence backwards
+tween.PlayBackwards();
 
 // set ID to manipulate single tween or mass manipulate tweeners 
 tween.SetId("MyID");
