06b0110dc25453f6a206787ee049b991c362992b
Alex <alexander@vourtsis.com>
2020-05-15 14:31:15 +0300

    Update Animation.cs

--- Animation.cs
+++ Animation.cs
@@ -31,15 +31,14 @@ using UnityEngine;
 	9.  With the GameObject still selected, choose Window > Animation > Animation
 	10. To the top left of the panel there should be a dropdown with all the assigned animations in the state machine
 	11. Choose an animation and then click on "Add Property", animate/change the property you wish
-	
-	note: Dragging and dropping a series of images in Unity "Scene" tab will create a Sprite animation that you can use for these
-		  states and change their frame timing as you wish from the Animation window
 
 	note: If you want a custom default state, you can right click anywhere in the Animator window, create a New State
 	      right click on the new state and choose "Set as Layer Default State"
 
-	note: In an Animation, to add a default keyframe, you have to right click on the Property you want to animate and "Add Key" 
-	      or hit the "K" key. Remember to do this before animating anything
+	note: In an Animation, to add a default keyframe you first have to add the property you intent to animate
+	
+	note: To add a keyframe, you can right click on the property you need to animate and "Add Key" or hit the "K" key with 
+	      the Animation window in focus. Or you can enable frame Recording and change the property you need to animate
 	
 */
 
@@ -72,3 +71,40 @@ if (Input.GetKeyDown(KeyCode.Space) && animStateInfo.shortNameHash == idleState)
 	stateAnimator.Play(moveState);
 }
 
+
+/* -----------------------------------------
+   Animate Sprites
+----------------------------------------- */
+
+/*
+
+	To animate a series of sprites with Unity's Animation system there's a couple ways:
+
+	a. Spritesheet
+	
+	1. Export your frames as a spritesheet from your animation program, make sure the frames are not trimmed
+       & are aligned in a grid with each frame being the same size
+	2. Import the spritesheet image and open Unity's Sprite Editor. At the top of that window choose to Slice
+       the image either by columns/rows or frame width and height. Apply changes
+	3. Create a new Animation and an Animator for the spritesheet
+	4. Click to expand the spritesheet sub-sprites/frames, select them and drag and drop the frames into the Animation timeline
+	5. While in the Animation window, click at the top right menu button and choose "Set Sample Rate" (or also "Show Sample Rate")
+	   so you can see and change the framerate of the animation
+	   
+	   
+	b. Individual Frames
+	
+	1. Export your frames as individual images
+	2. Import the images in a folder inside your project
+	3. Create a new Animation and an Animator for the frames
+	4. Select and drag and drop the frames into the Animation timeline
+	5. While in the Animation window, click at the top right menu button and choose "Set Sample Rate" (or also "Show Sample Rate")
+	   so you can see and change the framerate of the animation
+	6. You can also pack said images with a Sprite Atlas (Create > Sprite Atlas and drag and drop the image frames under "Objects for Packing")
+	   for better performance
+	   * Tip: You can drag & drop multiple textures by dragging them over the "Objects for Packing" title label
+	
+	note: Dragging and dropping a series of images in Unity "Scene" tab will prompt you to create a Unity Animation & Animator asset 
+	      that can be used to animate these images
+		  
+*/
