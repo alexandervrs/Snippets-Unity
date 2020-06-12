7ddaebc5746a14545296b39033c2c8998912519b
Alex <alexander@vourtsis.com>
2020-06-12 17:25:52 +0300

    Update Video.cs

--- Video.cs
+++ Video.cs
@@ -25,7 +25,9 @@ AudioSource movieAudio = GameObject.Find("Main Camera").AddComponent<AudioSource
 
 // adjust video settings
 moviePlayer.playOnAwake = false;                           // start immediately or wait
+moviePlayer.waitForFirstFrame = true;                      // whether the player will wait for first frame before playback, used when playOnAwake is on
 moviePlayer.renderMode  = VideoRenderMode.CameraNearPlane; // VideoRenderMode.CameraFarPlane will make the video show behind other GameObjects
+moviePlayer.skipOnDrop  = true;                            // if player is allowed to frameskip in order to be synced better with the game
 moviePlayer.targetCameraAlpha = 1.0f;                      // will make the video texture transparent
 moviePlayer.playbackSpeed = 1.0f;						   // set the playback speed
 moviePlayer.isLooping = false;							   // set loop playback
@@ -39,6 +41,9 @@ moviePlayer.prepareCompleted += OnVideoPrepared;
 // play the video and invoke callback "OnVideoFinished" when video ends. Might want to play the video when player is prepared
 moviePlayer.Play();                                       
 moviePlayer.loopPointReached += OnVideoFinished;
+
+// invoke "OnVideoError" when an loading error occurs, e.g. HTTP error if loading the video from a remote source
+moviePlayer.errorReceived += OnVideoError;
 
 // add an event for video prepared callback
 void OnVideoPrepared(VideoPlayer vp)
@@ -58,3 +63,37 @@ void OnVideoFinished(VideoPlayer vp)
 
 }
 
+// add an event for video error callback
+void OnVideoError(VideoPlayer vp, string message)
+{
+
+	Debug.LogError($"Error while loading the video: {message}");
+
+	vp.errorReceived -= OnVideoError; // unregister to avoid memory leaks
+
+}
+
+
+/* -----------------------------------------
+   Get Video Info
+----------------------------------------- */
+// is video playing
+bool isVideoPlaying = moviePlayer.isPlaying;
+
+// is video paused
+bool isVideoPlaying = moviePlayer.isPaused;
+
+// is video prepared? Will still report false if there's a loading error, check "errorReceived" for any errors
+bool isVideoPrepared = moviePlayer.isPrepared;
+
+// get video current position time
+double videoCurrentPosition = movieplayer.time;
+
+// get video duration
+float videoDuration = moviePlayer.length;
+
+// the current frame index
+int frameIndex = moviePlayer.frame;
+
+// the total frames of the video
+int totalFrames = moviePlayer.frameCount;
